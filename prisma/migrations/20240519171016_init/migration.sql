79f1044776c571451e3f790d25a6873d1eafa501
Rohit Singh <rohit.singh.33521@gmail.com>
2024-05-20 13:38:09 +0530

    update

--- prisma/migrations/20240519171016_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Book" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "summary" TEXT NOT NULL,
-    "publishedDate" DATETIME NOT NULL
-);
